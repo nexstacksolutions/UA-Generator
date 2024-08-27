c0f0be9eb080bd6dd04855762e7a674b5e419452
Zeeshan Asghar <mzeeshanff10@gmail.com>
2024-08-27 19:21:50 +0500

    Deploy latest code

--- src/context/UAContext.jsx
+++ src/context/UAContext.jsx
@@ -22,7 +22,7 @@ export const UAProvider = ({ children }) => {
   }, []);
 
   const toggleGenerator = React.useCallback(() => {
-    displayStatus("this option remove from admin", "success");
+    displayStatus("this option remove from admin", "error");
   }, [displayStatus]);
 
   const values = React.useMemo(
